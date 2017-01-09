895df762b442f93dee1262abf2697c997043a9d7
wzr <wzr@wzr-no-MacBook-Pro.local>
2017-01-09 22:57:30 +0900

    Add makefile

---
+++ StaticFramework/StaticFramework/StaticFramework.h
@@ -0,0 +1,15 @@
+//
+//  StaticFramework.h
+//  StaticFramework
+//
+//  Created by wzr on 2017/01/09.
+//  Copyright © 2017年 wzr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticFramework : NSObject
+
+- (void)testFramework;
+
+@end
---
+++ StaticFramework/StaticFramework/StaticFramework.m
@@ -0,0 +1,17 @@
+//
+//  StaticFramework.m
+//  StaticFramework
+//
+//  Created by wzr on 2017/01/09.
+//  Copyright © 2017年 wzr. All rights reserved.
+//
+
+#import "StaticFramework.h"
+
+@implementation StaticFramework
+
+- (void)testFramework{
+    NSLog(@"test framework");
+}
+
+@end
